248b51c516216baa1698be53d4dd7aae77ecf33e
Egon Willighagen <egon.willighagen@gmail.com>
2011-03-15 07:15:31 +0100

    Doing the QSAR descriptor analysis on the subset

--- projects/cmap/calcDesc.R
+++ projects/cmap/calcDesc.R
@@ -1,14 +1,23 @@
 # options(java.parameters = c("-Xmx4000m"))
 library(rcdk)
 
+compounds = read.csv("Compounds.csv")
+names = compounds[1:224,c("CMAP_chemical_name")]
+
 descNames <- unique(unlist(sapply(
   get.desc.categories(), get.desc.names
 )))
 
-iter <- iload.molecules("CMAP_ALL.sdf", type = "sdf")
+iter <- iload.molecules("CMAP_ALL_names.sdf", type = "sdf")
+x = c()
+xNames = c()
 while (hasNext(iter)) {
   mol <- nextElem(iter)
-  allDescs <- eval.desc(mol, descNames)
-  x = rbind(x, allDescs)
-  print(get.property(mol, "cdk:Title"))
+  name <- get.property(mol, "cdk:Title")
+  if (name %in% names) {
+    allDescs <- eval.desc(mol, descNames)
+    x = rbind(x, allDescs)
+    xNames = c(xNames, name)
+    print(name)
+  }
 }
