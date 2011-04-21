8c4eefc0115fec00ce2b6a8dd0482bb7344a0027
Egon Willighagen <egon.willighagen@gmail.com>
2011-04-21 10:37:11 +0200

    Chiral signatures based modeling of GI_50

---
+++ projects/cmap/signqsar/getChemBankID.groovy
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.BitSet;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.IStereoElement;
+import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+import org.openscience.cdk.signature.AtomSignature;
+
+fileName = "../data/CMAP_ALL.sdf"
+
+bits = new HashMap<String,Integer>();
+height = 1
+bitCount = 0;
+
+file = new File(fileName)
+println "# " + file + ":"
+iterator = new IteratingMDLReader(
+  file.newReader(),
+  NoNotificationChemObjectBuilder.getInstance()
+)
+while (iterator.hasNext()) {
+  IMolecule mol = iterator.next()
+  println mol.getProperty(CDKConstants.TITLE);
+}
+
+
