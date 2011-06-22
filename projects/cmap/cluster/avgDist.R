f3f0f0d548fee0a10af0e70620a5fa46f6c703cf
Egon Willighagen <egon.willighagen@gmail.com>
2011-06-22 07:24:29 +0200

    Average similarity is low

---
+++ projects/cmap/cluster/avgDist.R
@@ -0,0 +1,27 @@
+#options(java.parameters = c("-Xmx4000m"))
+library(rcdk)
+
+compounds = read.csv("../data/Compounds.csv")
+names = compounds[1:224,c("CMAP_chemical_name")]
+
+mols <- load.molecules(c("../data/CMAP_ALL_names.sdf"))
+print(paste("number of molecules: ", length(mols)))
+
+smallMols = c()
+
+for (mol in mols) {
+  name <- get.property(mol, "cdk:Title")
+  if (name %in% names) {
+    smallMols = c(smallMols, mol)
+  }
+}
+print(paste("number of molecules: ", length(smallMols)))
+
+fps <- lapply(smallMols, get.fingerprint, type='standard')
+sim.matrix = fp.sim.matrix(fps)
+fp.dist = 1 - sim.matrix
+
+clustering = hclust(as.dist(fp.dist), method="complete")
+plot(clustering, hang=-1)
+identified = identify(clustering)
+view.molecule.2d(smallMols[identified[[1]]])
