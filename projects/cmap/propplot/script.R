f76585feb8094abe8fbdc5d7e7b0a2e994472e25
Egon Willighagen <egon.willighagen@gmail.com>
2011-08-19 10:12:59 +0200

    Tuned SPARQL; picked RoF descriptor to plot

--- projects/cmap/propplot/script.R
+++ projects/cmap/propplot/script.R
@@ -1,22 +1,23 @@
 #options(java.parameters = c("-Xmx4000m"))
-library(rcdk)
-load("../rdf/cbmMols.R")
+source("../rdf/cbmMols.R")
 source("../../cdkpropdist/plot.propDist.R")
 
-alldata = getChemBioModData()
+alldata = getChemBioModData("../rdf")
+summarize.rdf(alldata)
 
 query = "
 SELECT ?title ?PC3 WHERE {
   ?compound <http://purl.org/dc/terms/title> ?title ;
-    <http://egonw.imm.ki.se/chembiomod/nci60-prop/hasMeasurement> ?m1 .
-  ?m1 <http://egonw.imm.ki.se/chembiomod/nci60-prop/hasValue> ?PC3 ;
-    <http://egonw.imm.ki.se/chembiomod/nci60-prop/inCellLine> ?line .
-  FILTER regex(?line, \"PC3\")
+    <http://egonw.imm.ki.se/chembiomod/nci60-prop/hasMeasurement> ?measurement .
+  ?measurement
+    <http://egonw.imm.ki.se/chembiomod/nci60-prop/inCellLine> \"PC3\" ;
+    <http://egonw.imm.ki.se/chembiomod/nci60-prop/hasValue> ?PC3 .
 }
 "
 
 pc3 = sparql.rdf(alldata, query, rowvarname="title")
 
+library(rcdk)
 mols <- load.molecules(molfiles=c("../data/CMAP_ALL_names.sdf"))
 
 molNames = lapply(mols, get.title)
@@ -29,6 +30,6 @@ plot.propdist(
   mols,
   selections=list(toxic, nontoxic, all),
   plotColors=c("red", "green", "black"),
-  descriptor="org.openscience.cdk.qsar.descriptors.molecular.WeightDescriptor",
-  main="", xlab="Weight"
+  descriptor="org.openscience.cdk.qsar.descriptors.molecular.RuleOfFiveDescriptor",
+  main="", xlab="Rule of Five"
 )
